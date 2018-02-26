c135b23fde6ca28fb09551d4221b7e4ae2d491d6
Vizena2005 <vizena2005@gmail.com>
2018-02-25 19:48:38 -0600

    finishing the question/answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,34 @@
 $(document).ready(function () {
 
+    // Global Variables
+    var questions = [{
+        "question": "In Majora's Mask, what are the 4 forms Link can change into via masks?",
+        "option1": "Horse, Gorgon, Epona, and God",
+        "option2": "Deku, Goron, Zora, and Fierce Deity Link",
+        "option3": "Epona, Zora, Goron, and Future Link",
+        "option4": "Zora, Goron, Sheik, and Midna",
+        "answer": "2"
+    }, {
+        "question": "In Ocarina of Time, what is the name of the forest where the Great Deku Tree lives?",
+        "option1": "Macalania",
+        "option2": "Kongo",
+        "option3": "Guardia",
+        "option4": "Kokiri",
+        "answer": "4"
+    }, {
+        "question": "In Twilight Princess, what is Midna weak against?",
+        "option1": "Water",
+        "option2": "Fire",
+        "option3": "Light",
+        "option4": "Darkness",
+        "answer": "3"
+    },{
+        "question": "In Wind Waker where did Link find Tingle?",
+        "option1": "Ice Ring Isle",
+        "option2": "Forest Haven",
+        "option3": "Windfall Island",
+        "option4": "Great Fish Isle",
+        "answer": "3"
+    }]
+// Document Ready Closing Function
 });
