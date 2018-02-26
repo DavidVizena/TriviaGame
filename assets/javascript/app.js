888cb4b5a9a63f90b15460382769d42447431963
Vizena2005 <vizena2005@gmail.com>
2018-02-25 23:42:23 -0600

    functionality very close to being finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
     // Global Variables
     // 
-    // Question
+    // Questions
     var questions = [{
         "question": "In Majora's Mask, what are the 4 forms Link can change into via masks?",
         "option1": "Horse, Gorgon, Epona, and God",
@@ -34,16 +34,55 @@ $(document).ready(function () {
     }];
 
     var currentQuestion = 0;
-    var score = 0;
+    var correct = 0;
+    var incorrect = 0;
+    var unanswered = 0;
+    var time = 30;
     var totQuestions = questions.length;
-    
-    var container = $("#quizContainer");
-    var zeldaQuestion = $("#question");
-    var opt1 = $("#opt1");
-    var opt2 = $("#opt2");
-    var opt3 = $("#opt3");
-    var opt4 = $("#opt4");
-    
 
+    var container = document.getElementById('quizContainer');
+    var resultWrapper = document.getElementsByClassName('resultWrapper');
+    var zeldaQuestion = document.getElementById('question');
+    var opt1 = document.getElementById('opt1');
+    var opt2 = document.getElementById('opt2');
+    var opt3 = document.getElementById('opt3');
+    var opt4 = document.getElementById('opt4');
+    var inputs = document.getElementsByTagName('input');
+    var resultCorrect = document.getElementById('correct');
+    var resultIncorrect = document.getElementById('incorrect');
+    var resultUnanswered = document.getElementById('unanswered');
+    // Gets a question by injecting the current question into the HTML
+    function getQuestion(questionIndex) {
+        var q = questions[questionIndex];
+        zeldaQuestion.textContent = (questionIndex + 1) + ". " + q.question;
+        opt1.textContent = q.option1;
+        opt2.textContent = q.option2;
+        opt3.textContent = q.option3;
+        opt4.textContent = q.option4;
+    };
+    // Tests an answer to a question against the saved correct answer value and awards points accordingly
+    function rightWrong() {
+        var answer = inputs.value;
+        if (questions[currentQuestion].answer == answer) {
+            correct++;
+        }
+        if (questions[currentQuestion].answer !== answer) {
+            incorrect++;
+        }
+        if (time == 0) {
+            unanswered++;
+        }
+        currentQuestion++;
+        if (currentQuestion == totQuestions) {
+            container.style.display = 'none';
+            resultWrapper.style.display = '';
+            resultCorrect.textContent = "Correct: " + correct;
+            resultIncorrect.textContent = "Incorrect: " + incorrect;
+            resultUnanswered.textContent = "Correct: " + unanswered;
+            return;
+        }
+        getQuestion(currentQuestion);
+    }
+    getQuestion(currentQuestion);
     // Document Ready Closing Function
 });
