1561f56bc9b619ccc06de099c563ea4f25c7fb60
Vizena2005 <vizena2005@gmail.com>
2018-03-02 15:37:33 -0600

    lowering navi Talking

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -101,7 +101,7 @@ $(document).ready(function () {
             countdown();
             getQuestion(currentQuestion);
             getBackground();
-        }, 2000);
+        }, 1000);
         setTimeout(function () {
             $(this).prop('disabled', false);
         }, 2000);
